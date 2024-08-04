435a0234760f484ca4342be8d48195b2307df9c3
ezeabregu <ezeabregu.ea@gmail.com>
2024-08-04 19:18:54 -0300

    changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,6 +13,10 @@ const Home = () => {
         que cada voz cuenta y que a traves de opiniones y experiencias se puede
         conocer un barrio.
       </p>
+      <p>
+        Únete y descubre lo que otros piensan y comparte tus propias historias
+        del barrio.
+      </p>
     </ContainerHome>
   );
 };
