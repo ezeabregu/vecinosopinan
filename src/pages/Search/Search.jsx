5eddfbeacc4b6c64f4a4475a12153e69ce5d151d
ezeabregu <ezeabregu.ea@gmail.com>
2024-08-10 22:16:58 -0300

    some changes

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,23 +1,21 @@
 import React from "react";
-import { ContainerSearch } from "./searchStyles";
+import { ContainerSearch, MapResponsive } from "./searchStyles";
 import Iframe from "react-iframe";
 
 const Search = () => {
   return (
     <ContainerSearch>
-      {Iframe ? (
-        <Iframe
-          url="https://www.google.com/maps/d/u/0/viewer?mid=1jOvKnOgVTvHtdZODbsv_Hh59ixpm_dY&ll=-31.3822691176836%2C-64.20128424999999&z=11"
-          width="640"
-          height="480"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        ></Iframe>
-      ) : (
-        <p>Cargando mapa...</p>
-      )}
+      <MapResponsive>
+        {Iframe ? (
+          <Iframe
+            src="https://www.google.com/maps/d/embed?mid=1jOvKnOgVTvHtdZODbsv_Hh59ixpm_dY&ehbc=2E312F"
+            // width="640"
+            // height="480"
+          ></Iframe>
+        ) : (
+          <p>Cargando mapa...</p>
+        )}
+      </MapResponsive>
     </ContainerSearch>
   );
 };
--- src/pages/Search/searchStyles.js
+++ src/pages/Search/searchStyles.js
@@ -8,4 +8,25 @@ export const ContainerSearch = styled.div`
   color: var(--blanco);
   display: flex;
   justify-content: center;
+
+  p {
+    color: var(--blanco);
+  }
+`;
+
+export const MapResponsive = styled.div`
+  overflow: hidden;
+  padding-bottom: 400px;
+  position: relative;
+  height: 80vh;
+  width: 80vw;
+  max-width: 1300px;
+
+  Iframe {
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+  }
 `;
