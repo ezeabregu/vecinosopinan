ccce9110b01fac181ddc0072e71b26829d39ef83
ezeabregu <ezeabregu.ea@gmail.com>
2024-09-30 11:32:33 -0300

    account changes

--- src/pages/Accounts/Account/Account.jsx
+++ src/pages/Accounts/Account/Account.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ContainerAccount } from "./accountStyles";
+import { ContainerAccount, ContainerAccountTitle } from "./accountStyles";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentUser } from "../../../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
@@ -10,15 +10,17 @@ const Account = () => {
   const dispatch = useDispatch();
   return (
     <ContainerAccount>
-      <h2>Bienvenido {currentUser.name}</h2>
-      <button
-        onClick={() => {
-          dispatch(setCurrentUser(null));
-          navigate("/");
-        }}
-      >
-        Salir
-      </button>
+      <ContainerAccountTitle>
+        <h2>Hola {currentUser.name}</h2>
+        <button
+          onClick={() => {
+            dispatch(setCurrentUser(null));
+            navigate("/");
+          }}
+        >
+          Salir
+        </button>
+      </ContainerAccountTitle>
     </ContainerAccount>
   );
 };
--- src/pages/Accounts/Account/accountStyles.js
+++ src/pages/Accounts/Account/accountStyles.js
@@ -4,3 +4,23 @@ export const ContainerAccount = styled.div`
   height: calc(100vh - 80px);
   padding: 1rem;
 `;
+
+export const ContainerAccountTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+
+  button {
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    background-color: var(--amarilloOscuro);
+    color: var(--blanco);
+    font-size: 1rem;
+    padding: 5px 20px;
+    &:hover {
+      background-color: var(--amarillo);
+    }
+  }
+`;
